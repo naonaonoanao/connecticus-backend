82863af28798543200ab694d1f246a4b02ff4ae7
nao <egorovanasta226@gmail.com>
2025-05-02 01:46:36 +0700

    added back for graph

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ load_dotenv()
 from app.core.config import settings
 from fastapi import FastAPI
 from app.core.database import engine
-from app.api.v1 import employee, user
+from app.api.v1 import employee, user, graph
 from fastapi import APIRouter
 
 from app.db.create_tables import create_tables
@@ -23,6 +23,7 @@ def get_application() -> FastAPI:
     v1_router = APIRouter(prefix='/api/v1')
     v1_router.include_router(employee.router)
     v1_router.include_router(user.router)
+    v1_router.include_router(graph.router)
 
     app.include_router(v1_router)
 
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from uuid import UUID
 from datetime import date
 
@@ -38,6 +38,23 @@ class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
 
+
 class LoginDTO(BaseModel):
     username: str
     password: str
+
+
+class GraphNode(BaseModel):
+    id: str
+    name: str
+    group: str
+
+
+class GraphLink(BaseModel):
+    source: str
+    target: str
+
+
+class GraphViewDTO(BaseModel):
+    nodes: List[GraphNode]
+    links: List[GraphLink]
